6fcffa5335f7124e5ade8109995f3477b4312c92
Patrick Love <plove13x@gmail.com>
2014-09-09 23:41:35 -0400

    Initial Commit

---
+++ scratch.js
@@ -0,0 +1,35 @@
+//but need to do for each result add new itemsquare first...
+
+/*
+$(‘.itemSquare’).each(function(index) {
+$(this).text(new Date);
+});
+*/
+
+
+
+/*
+results.forEach(function(item) {
+
+
+function addClass() {
+  var $newItem = $('li');
+  $theDivs.addClass('cool')
+    .width('100px')
+    .height('100px');
+}
+
+$('#shop').append(item."Shop"."shop_name");
+});
+*/
+
+
+
+/*
+for ("shop_name" in "Shop" in "results" in etsyResults) {
+
+create new li
+
+$('#shop').append(item."Shop"."shop_name")
+};
+*/
